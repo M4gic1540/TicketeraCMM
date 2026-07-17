a5488cbece8e2145f5b2a1684360933803514718
M4gic1540 <t.gonzalezb24@gmail.com>
2026-07-16 23:17:02 -0400

    Creacion de modulos

--- ticketera/src/app.module.ts
+++ ticketera/src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TicketsModule } from './tickets/tickets.module';
+import { IdentityModule } from './identity/identity.module';
+import { AssetsModule } from './assets/assets.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, TicketsModule, IdentityModule, AssetsModule, NotificationsModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
